1cad535029ae1d159afc12a880b4e52fc59120f2
Ahmad Elwan <ahmadmelwan02@gmail.com>
2024-01-08 16:22:10 +0300

    Added authentication routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = require("express").Router();
+const { register, login } = require("../controllers/auth");
 
 
+// Register a new user
 
-router.get("/", (req, res) => {
+router.post("/register", register);
 
-})
+// Login as an existing user
 
+router.post("/login", login);
 
 
 
